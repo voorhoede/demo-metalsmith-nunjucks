6b5ad2650b7c09278b88a95348753801ad1aab13
Jasper Moelker <j.b.moelker@activeids.nl>
2017-08-31 09:52:52 +0200

    Use own metalsmith-nunjucks for rendering
    * This replaces -in-place and -layouts plugins.
    * This allows for the Nunjucks render environment to be extended.
    * Render env is extended with date filter.
    
    To do: make async? add error handling.

--- build.js
+++ build.js
@@ -2,15 +2,16 @@ const Metalsmith = require('metalsmith')
 const assets = require('metalsmith-assets')
 const chalk = require('chalk')
 const collections = require('metalsmith-collections')
-const inplace = require('metalsmith-in-place')
+const dateFilter = require('nunjucks-date-filter')
 const layoutByCollection = require('./lib/layout-by-collection')
-const layouts = require('metalsmith-layouts')
 const markdown = require('metalsmith-markdown')
 const metadata = require('metalsmith-metadata')
+const metalNunjucks = require('./lib/metalsmith-nunjucks')
 const nunjucks = require('nunjucks')
 const permalinks = require('metalsmith-permalinks')
 
-nunjucks.configure('src', {watch: false, noCache: true})
+const env = new nunjucks.Environment(new nunjucks.FileSystemLoader('src'), {watch: false, noCache: true})
+env.addFilter('formatDate', dateFilter)
 
 Metalsmith(__dirname)
   .source('./data')
@@ -22,11 +23,12 @@ Metalsmith(__dirname)
     site:  'site.json'
   }))
   .use(collections({
-      posts: {
-        pattern: 'posts/**/*.md',
-        sortBy: 'date',
-        reverse: true
-      }}))
+    posts: {
+      pattern: 'posts/**/*.md',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
   .use(markdown())
   .use(permalinks({
     pattern: ':title'
@@ -34,14 +36,12 @@ Metalsmith(__dirname)
   .use(layoutByCollection({
     'posts': 'post.html'
   }))
-  .use(inplace({
-    engine: 'nunjucks',
-    pattern: '**/*.html'
+  .use(metalNunjucks.renderContents({ 
+    env
   }))
-  .use(layouts({
-    engine: 'nunjucks',
-    pattern: '**/*.html',
-    directory: 'src/views'
+  .use(metalNunjucks.renderLayout({
+    env,
+    directory: 'views'
   }))
   .build((err) => {
     if (err) {
